b6b42aac03cb15ee8038cee682d926013f05f69f
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-05-31 11:26:40 +0300

    updated

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+  def actor
+    super
+  end
+
+  def show
+    super
+  end
+
+  def say_that_thing_you_say
+    "#{name} always says:#{catchphrase}"
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+has_many :characters
+has_many :actors, through: :characters
+def characters
+    super
+end  
+
+def network
+    super
+end
+
+def actors_list
+    actors.map(&:full_name)
+end
 end
